a99189fa90b36abaaafa7f86a912c52b8e948da9
kauamedeiros16 <kaua.23044@aluno.iffar.edu.br>
2026-03-03 14:59:22 -0300

    Add files via upload

---
+++ vet3.php
@@ -0,0 +1,17 @@
+<?php 
+
+$produto= [
+ "nome" => "faca tramontina",
+ "preço" => 68,
+ "estoque" => "10"
+];
+foreach ($produto as $chave => $valor ) {
+ echo " $chave: $valor <br>";
+}
+
+
+
+
+
+
+?>
